6565ce24ef57393044abd60ccd47740cc1128222
Luke Belbina <kaderbelbina@gmail.com>
2021-02-05 13:45:36 -0800

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,62 @@
+import fs from 'fs';
+import core from '@actions/core';
+import github from '@actions/github';
+
+function readFile(filename: string): string {
+  console.log(`Reading file ${filename}`);
+  const rawdata: Buffer = fs.readFileSync(filename);
+  return rawdata.toString();
+}
+
+// expected format 0.0.0
+function bumpVersion(version: string) {
+  const split = version.split('.');
+  const versionBump = split[0] + '.' + split[1] + '.' + (Number.parseInt(split[2]) + 1);
+  return versionBump;
+}
+
+function bumpChart(chartYaml: string): string {
+  const chartVersionMatch = chartYaml.match(/(?<=version: )\d+.\d+.\d+/gm);
+  if (chartVersionMatch == null) {
+    throw new Error('No chart version found in file');
+  } else if (chartVersionMatch.length > 1) {
+    throw new Error('Multiple chart versions found in file. This is currently not handled.');
+  }
+
+  const appVersionMatch = chartYaml.match(/(?<=appVersion: '|")\d+.\d+.\d+/);
+  if (appVersionMatch == null) {
+    throw new Error('No app version found in file');
+  } else if (appVersionMatch.length > 1) {
+    throw new Error('Multiple app versions found in file. This is currently not handled.');
+  }
+
+  const appVersion = appVersionMatch[0];
+  const chartVersion = chartVersionMatch[0];
+
+  const appVersionBump = bumpVersion(appVersion);
+  const chartVersionBump = bumpVersion(chartVersion);
+
+  return chartYaml.replace(appVersion, appVersionBump).replace(chartVersion, chartVersionBump);
+}
+
+//const file = readFile('example-helm-chart.yml');
+//const match = bumpChart(file);
+//console.log(match);
+
+async function run() {
+  const githubToken = core.getInput('token');
+
+  console.log('Hello World!');
+
+  //   const inputFileName = core.getInput('input_file');
+
+  //   const file = readFile(inputFileName);
+
+  //   const bumpedFile = bumpChart(file);
+
+  //   //const octokit = github.getOctokit(githubToken);
+
+  //   console.log(bumpedFile);
+}
+
+run().catch((error) => core.setFailed('Workflow failed! ' + error.message));
